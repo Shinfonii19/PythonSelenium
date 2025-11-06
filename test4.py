3adbc2eed09d8883f41e5d093463de8b9c3d9f97
Ron Jerald Capiral <jeraldcapiral123@gmail.com>
2025-11-06 20:51:40 +0800

    Just testing the git command

--- test4.py
+++ test4.py
@@ -1,1 +1,10 @@
-print("Hello")
+from selenium import webdriver
+
+# setup a function for webdriver
+driver = webdriver.Chrome()
+
+# wait up to 10 secs
+driver.implicitly_wait(10)
+
+# open a link
+driver.get("https://google.com")
