31d787fb2ce46e3e3746b2da3b8da730151e875d
Ron Jerald Capiral <jeraldcapiral123@gmail.com>
2025-11-06 21:15:04 +0800

    Implicit Wait

--- test4.py
+++ test4.py
@@ -8,3 +8,9 @@ driver.implicitly_wait(10)
 
 # open a link
 driver.get("https://google.com")
+
+# finding a element with a value of 'q' on the name (name = q)
+search_box = driver.find_element("name", "q")
+
+# sending a keys, value or text
+search_box.send_keys("Selenium waits example")
